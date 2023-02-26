e08ecc86213fda9fa8f0a9b7eb46789308a6c958
navneetguptacse <navneetguptaid7@gmail.com>
2023-02-26 16:12:15 +0530

    LTS-commit

---
+++ one-time.cmd
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "my-git commit"
+git config --global user.email "navneetguptaid7@gmail.com"
+git config --global user.name "navneetguptacse"
+git branch -M main
+git remote remove origin
+git remote add origin https://github.com/navneetguptacse/Web-Scraping-ML.git
+git push -u origin main
